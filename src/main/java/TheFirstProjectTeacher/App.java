f7a4ea1a20b7ffb332e4013274dd5302d1661b5b
LIMLOSAS <gudwns3721@gmail.com>
2023-03-02 11:32:22 +0900

    Theacher2

--- src/main/java/TheFirstProjectTeacher/App.java
+++ src/main/java/TheFirstProjectTeacher/App.java
@@ -16,8 +16,14 @@ public class App {
             System.out.print("명령) ");
             String command = sc.nextLine().trim();
             //trim : 좌우 공백 제거
+
             if (command.equals("종료")) {
                 break;
+            } else if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = sc.nextLine().trim();
+                System.out.print("작가 : ");
+                String authorName = sc.nextLine().trim();
             }
         }
     }
